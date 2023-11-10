d79383d3f60d8a20a41bf181e0c006efb536f25a
Nutty4616 <nkosin361@gmail.com>
2023-11-10 16:07:34 +0200

    Updating product details

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -39,6 +39,18 @@ function App() {
       background: {
         default: paletteType === "light" ? "#eaeaea" : "#121212",
       },
+      primary: {
+        light: "#757ce8",
+        main: "#3f50b5",
+        dark: "#002884",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#ff7961",
+        main: "#f44336",
+        dark: "#ba000d",
+        contrastText: "#000",
+      },
     },
     components: {
       MuiCssBaseline: {
