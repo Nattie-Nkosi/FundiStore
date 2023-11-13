fa60a8868f47954ec86fe3a547a8ec30bf88a959
Nutty4616 <nkosin361@gmail.com>
2023-11-13 19:58:09 +0200

    Using async functions in redux part 1

--- client/src/features/Cart/cartSlice.ts
+++ client/src/features/Cart/cartSlice.ts
@@ -1,13 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Cart } from "../../app/models/cart";
+import agent from "../../app/api/agent";
 
 interface CartState {
   cart: Cart | null;
+  status: string;
 }
 
 const initialState: CartState = {
-  cart: null
+  cart: null,
+  status: 'idle'
 }
+
+export const addCartItemAsync = createAsyncThunk<Cart, { productId: number, quantity: number }>(
+  'cart/addCartItemAsync',
+  async ({ productId, quantity }) => {
+    try {
+      return await agent.Cart.addItem(productId, quantity);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+);
 
 export const cartSilce = createSlice({
   name: 'cart',
@@ -24,7 +38,21 @@ export const cartSilce = createSlice({
       if (state.cart!.items[itemIndex].quantity === 0)
         state.cart?.items.splice(itemIndex, 1);
     }
-  }
+  },
+  extraReducers: (builder => {
+    builder.addCase(addCartItemAsync.pending, (state, action) => {
+      console.log(action);
+      state.status = 'pendingAddItem'
+    });
+    builder.addCase(addCartItemAsync.fulfilled, (state, action) => {
+      state.cart = action.payload;
+      state.status = 'idle'
+    });
+    builder.addCase(addCartItemAsync.rejected, (state) => {
+
+      state.status = 'idle'
+    });
+  })
 })
 
 export const { setCart, removeItem } = cartSilce.actions;
