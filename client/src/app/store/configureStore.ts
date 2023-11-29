b9e44cf9fd54d02c44b0dcd02268a74dc15d9d65
Nutty4616 <nkosin361@gmail.com>
2023-11-29 12:49:01 +0200

    Adding account Slice

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -3,12 +3,14 @@ import { counterSlice } from "../../features/Contact/counterSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { cartSlice } from "../../features/Cart/cartSlice";
 import { catalogSlice } from "../../features/Catalog/catalogSlice";
+import { accountSlice } from "../../features/account/accountSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     cart: cartSlice.reducer,
-    catalog: catalogSlice.reducer
+    catalog: catalogSlice.reducer,
+    account: accountSlice.reducer
   }
 });
 
