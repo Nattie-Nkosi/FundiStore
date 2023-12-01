c1b288ec81d6f58f35f60ec1d8cf924275f5b1f0
Nutty4616 <nkosin361@gmail.com>
2023-12-01 17:27:11 +0200

    Client side testing and code for basket transfer

--- client/src/app/layout/SignedInMenu.tsx
+++ client/src/app/layout/SignedInMenu.tsx
@@ -2,6 +2,7 @@ import { Button, Menu, Fade, MenuItem } from "@mui/material";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../store/configureStore";
 import { signOut } from "../../features/account/accountSlice";
+import { clearCart } from "../../features/Cart/cartSlice";
 
 export default function SignInMenu() {
   const dispatch = useAppDispatch();
@@ -34,7 +35,14 @@ export default function SignInMenu() {
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My orders</MenuItem>
-        <MenuItem onClick={() => dispatch(signOut())}>Logout</MenuItem>
+        <MenuItem
+          onClick={() => {
+            dispatch(signOut());
+            dispatch(clearCart());
+          }}
+        >
+          Logout
+        </MenuItem>
       </Menu>
     </>
   );
