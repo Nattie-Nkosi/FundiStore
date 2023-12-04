30562cbc28a4fc55f2dacb64d162bcb45258736c
Nutty4616 <nkosin361@gmail.com>
2023-12-04 18:02:54 +0200

    Creating the order entities

---
+++ API/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,9 @@
+namespace API.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
