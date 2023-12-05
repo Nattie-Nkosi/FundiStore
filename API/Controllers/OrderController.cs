fce4dc53da008dcb0d1a2c94c00268cd0a694470
Nutty4616 <nkosin361@gmail.com>
2023-12-05 16:35:48 +0200

    Adding the create order method

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,5 +1,8 @@
 using API.Data;
+using API.DTOs;
+using API.Entities;
 using API.Entities.OrderAggregate;
+using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +19,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public async Task<ActionResult<List<Order>>> GetOrders()
         {
             return await _context.Orders
@@ -24,6 +28,7 @@ namespace API.Controllers
                     .ToListAsync();
         }
 
+        [HttpGet("{id}", Name = "GetOrder")]
         public async Task<ActionResult<Order>> GetOrder(int id)
         {
             return await _context.Orders
@@ -31,5 +36,75 @@ namespace API.Controllers
                     .Where(x => x.BuyerId == User.Identity.Name && x.Id == id)
                     .FirstOrDefaultAsync();
         }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateOrder(CreateOrderDto orderDto)
+        {
+            var cart = await _context.Carts
+                    .RetrieveCartWithItems(User.Identity.Name)
+                    .FirstOrDefaultAsync();
+
+            if(cart == null) return BadRequest(new ProblemDetails{Title = "Could not the cart"});
+
+            var items = new List<OrderItem>();
+
+            foreach (var item in cart.Items)
+            {
+                var productItem = await _context.Products.FindAsync(item.ProductId);
+                var itemOrdered = new ProductItemOrdered
+                {
+                    ProductId = productItem.Id,
+                    Name = productItem.Name,
+                    PictureUrl = productItem.PictureUrl
+                };
+                var orderItem = new OrderItem
+                {
+                    ItemOrdered = itemOrdered,
+                    Price = productItem.Price,
+                    Quantity = item.Quantity 
+                };
+                items.Add(orderItem);
+                productItem.QuantityInStock -= item.Quantity;
+            }
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+            var deliveryFee = subtotal > 10000 ? 0 : 500;
+
+            var order = new Order
+            {
+                OrderItems = items,
+                BuyerId = User.Identity.Name,
+                ShippingAddress = orderDto.ShippingAddress,
+                Subtotal = subtotal,
+                DeliveryFee = deliveryFee
+            };
+
+            _context.Orders.Add(order);
+            _context.Carts.Remove(cart);
+
+            if(orderDto.SaveAddress)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                user.Address = new UserAddress
+                {
+                    FullName = orderDto.ShippingAddress.FullName,
+                    Address1 = orderDto.ShippingAddress.Address1,
+                    Address2 = orderDto.ShippingAddress.Address2,
+                    City = orderDto.ShippingAddress.City,
+                    State = orderDto.ShippingAddress.State,
+                    Zip = orderDto.ShippingAddress.Zip,
+                    Country = orderDto.ShippingAddress.Country,
+                };
+                _context.Update(user);
+            }
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if(result) return CreatedAtRoute("GetOrder", new {id = order.Id}, order.Id);
+
+            return BadRequest("Problem creating order");
+            
+        }
+        
     }
 }
--- API/Extensions/CartExtensions.cs
+++ API/Extensions/CartExtensions.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
@@ -22,6 +23,11 @@ namespace API.Extensions
                     Quantity = item.Quantity
                 }).ToList()
             };
+        }
+
+        public static IQueryable<Cart> RetrieveCartWithItems(this IQueryable<Cart> query, string buyerId)
+        {
+            return query.Include(i => i.Items).ThenInclude(p => p.Product).Where(b => b.BuyerId == buyerId);
         }
     }
 }
