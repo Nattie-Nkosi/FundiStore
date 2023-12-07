533cb124646bc4f22316edd5708cc3c88a13ab6c
Nutty4616 <nkosin361@gmail.com>
2023-12-07 11:58:22 +0200

    Adding a validation library

---
+++ client/src/features/checkout/checkoutValidation.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+export const validationSchema = yup.object({
+  fullname: yup.string().required('Full name is required'),
+  address1: yup.string().required('Address line 1 is required'),
+  city: yup.string().required(),
+  state: yup.string().required(),
+  zip: yup.string().required(),
+  country: yup.string().required(),
+})
