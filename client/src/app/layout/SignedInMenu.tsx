f32ab9a8d917bb85c1084ebd14af9d8787c6314d
Nutty4616 <nkosin361@gmail.com>
2023-12-11 19:39:34 +0200

    Creating the orders page

--- client/src/app/layout/SignedInMenu.tsx
+++ client/src/app/layout/SignedInMenu.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "../store/configureStore";
 import { signOut } from "../../features/account/accountSlice";
 import { clearCart } from "../../features/cart/cartSlice";
+import { Link } from "react-router-dom";
 
 export default function SignInMenu() {
   const dispatch = useAppDispatch();
@@ -34,7 +35,9 @@ export default function SignInMenu() {
         TransitionComponent={Fade}
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My orders</MenuItem>
+        <MenuItem component={Link} to="/orders">
+          My orders
+        </MenuItem>
         <MenuItem
           onClick={() => {
             dispatch(signOut());
